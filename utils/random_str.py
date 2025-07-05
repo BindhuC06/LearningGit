671eaf38d8f0b4bd8cecf302056d0e3b093e9e48
Krishnapriya <quantumplatores.moon02@gmail.com>
2025-07-05 16:53:01 +0530

    Update random_str.py

--- utils/random_str.py
+++ utils/random_str.py
@@ -1,4 +1,8 @@
 # TODO -  krsnapriya
+import random
+import string
 
 def get_random_string_of_length_6():
-    pass
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+
+print(get_random_string_of_length_6())
